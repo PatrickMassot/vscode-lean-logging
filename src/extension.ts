e4a809b4148d031843abbc4595d87d0e6aafb4b2
Patrick Massot <patrickmassot@free.fr>
2026-01-20 09:26:05 +0100

    Make things synchronous to avoid race conditions

--- src/extension.ts
+++ src/extension.ts
@@ -12,30 +12,30 @@ export function activate(context: vscode.ExtensionContext) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	const setRecordingContext = (isRecording: boolean) => {
-      vscode.commands.executeCommand('setContext', 'lean-logging:isRecording', isRecording);
+	const setRecordingContext = async (isRecording: boolean) => {
+      await vscode.commands.executeCommand('setContext', 'lean-logging:isRecording', isRecording);
     };
 
-	const disposable = vscode.commands.registerCommand('lean-logging.startLogging', () => {
+	const disposable = vscode.commands.registerCommand('lean-logging.startLogging', async () => {
 		// The code you place here will be executed every time your command is executed
 
 		// Display a message box to the user
 		vscode.window.showInformationMessage('Démarrage de l’enregistrement.');
-    setRecordingContext(true);
-    vscode.workspace.getConfiguration('lean4.logging').update('enabled', true, vscode.ConfigurationTarget.Global);
-    vscode.commands.executeCommand('lean4.restartServer');
+    await setRecordingContext(true);
+    await vscode.workspace.getConfiguration('lean4.logging').update('enabled', true, vscode.ConfigurationTarget.Global);
+    await vscode.commands.executeCommand('lean4.restartServer');
 	});
 
 	context.subscriptions.push(disposable);
 
-	const disposable2 = vscode.commands.registerCommand('lean-logging.stopLogging', () => {
+	const disposable2 = vscode.commands.registerCommand('lean-logging.stopLogging', async () => {
 		// The code you place here will be executed every time your command is executed
 
 		// Display a message box to the user
 		vscode.window.showInformationMessage('Arrêt de l’enregistrement.');
-    setRecordingContext(false);
-    vscode.workspace.getConfiguration('lean4.logging').update('enabled', false, vscode.ConfigurationTarget.Global);
-    vscode.commands.executeCommand('lean4.restartServer');
+    await setRecordingContext(false);
+    await vscode.workspace.getConfiguration('lean4.logging').update('enabled', false, vscode.ConfigurationTarget.Global);
+    await vscode.commands.executeCommand('lean4.restartServer');
 	});
 
 	context.subscriptions.push(disposable2);
